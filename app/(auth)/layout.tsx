0640762ea78ab1bd5ed5352914e893c079dcfb70
faiqbrinatta <zhafranbrinatta@gmail.com>
2026-05-05 17:00:09 +0700

    fiz conflict on (auth)

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client"
 import {motion} from "framer-motion"
 
@@ -14,15 +13,6 @@ const AuthLayout = ({children}: {children: React.ReactNode}) => {
       >
         {children}
       </motion.div>
-=======
-const AuthLayout = ({children}: {children: React.ReactNode}) => {
-  return (
-    <div className="bg-gray-100">
-      <div className="flex flex-col items-center justify-between px-6 py-8 mx-auto h-screen">
-        <div className="w-full bg-white rounded-lg shadow mt-0 max-w-md">
-            {children}
-        </div>
->>>>>>> origin/main
       </div>
     </div>
   )
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,14 +3,9 @@ import { GoogleButton } from "@/components/ui/social-button";
 
 const Login = () => {
   return (
-<<<<<<< HEAD
     <div className='p-6 space-y-4 bg-[#008FCC] rounded-tl-[50px] rounded-br-[50px]'>
       <h1 className='text-2xl text-center font-semibold  text-white'>WELCOME BACK</h1>
       <p className="text-center text-white"> Sign in to your Hismaya account</p>
-=======
-    <div className='p-6 space-y-4'>
-      <h1 className='text-2xl font-bold text-gray-900'>Sign In to your account</h1>
->>>>>>> origin/main
       <FormLogin />
       <div className="
           my-4 flex items-center 
@@ -18,11 +13,7 @@ const Login = () => {
           after:flex-1 after:border-t after:border-gray-300
         "
       >
-<<<<<<< HEAD
         <p className="mx-4 mb-0 text-center font-semibold text-white">Or</p>
-=======
-        <p className="mx-4 mb-0 text-center font-semibold text-gray-600">Or</p>
->>>>>>> origin/main
       </div>
       <GoogleButton />
     </div>
